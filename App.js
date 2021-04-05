dd43f43590f5347051ca0687bb35f26f28b4528f
hongdotcom <hongdotcom@gmail.com>
2021-04-06 10:33:30 +1200

    init basic

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  Image,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require("./assets/mobilebg.jpg")}
+      style={styles.bg}
+    >
+      {/* <StatusBar style="auto" /> */}
+      <Image
+        source={require("./assets/logo_transparent.png")}
+        style={styles.logo}
+      />
+      <TextInput style={styles.input} />
+      <TextInput style={styles.input} />
+      <TextInput />
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  bg: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: "25%",
+    height: "25%",
+    position: "absolute",
+    top: 120,
+  },
+  input: {
+    height: 60,
+    backgroundColor: "#fff",
+    width: "80%",
+    margin: "1%",
+    borderWidth: 1,
+    display: "flex",
   },
 });
